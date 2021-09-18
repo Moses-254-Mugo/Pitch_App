bce301d67d60b6f8591ea0b6da7b0b8e939949ef
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-19 00:39:51 +0300

    Add views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,24 +1,24 @@
-# from flask import render_template, redirect, url_for
-# from . import main
-# from .forms import ReviewForm
+from flask import render_template, redirect, url_for
+from . import main
+from .forms import ReviewForm
 
-# # Views
-# @main.route('/')
-# def index():
+# Views
+@main.route('/')
+def index():
 
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     message = 'Moses Kinyua Mugo'
-#     title = 'Home - welcome to the Best Movie Reviews Website Online'
+    '''
+    View root page function that returns the index page and its data
+    '''
+    message = 'Moses Kinyua Mugo'
+    title = 'Home - welcome to the Best Movie Reviews Website Online'
 
-#     return render_template('index.html', message = message, title = title)
+    return render_template('index.html', message = message, title = title)
 
 
-# @main.route('/movie/<int:movie_id>')
-# def movie(movie_id):
+@main.route('/movie/<int:movie_id>')
+def movie(movie_id):
 
-#     '''
-#     View movie page function that returns the movie details page and its data
-#     '''
-#     return render_template('movie.html')
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html')
