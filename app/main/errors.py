61f4d378bbb4b7a89df9e750797179f1522b8c7d
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-19 00:40:39 +0300

    Add error fourOwfour file

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,12 +1,12 @@
-# from flask import render_template
-# from . import main
+from flask import render_template
+from . import main
 
-# @main.app_errorhandler(404)
-# def four_Ow_four(error):
+@main.app_errorhandler(404)
+def four_Ow_four(error):
     
-#     '''
+    '''
 
-#     Function to render the 404 error page.
+    Function to render the 404 error page.
 
-#     '''
-#     return render_template('404.html'),404
+    '''
+    return render_template('404.html'),404
