e88cfc0cdf62c20962117d6d45255fcefdf75ead
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-19 11:08:46 +0300

    Setup SMTP server and cofigure ports

--- config.py
+++ config.py
@@ -1,10 +1,22 @@
-
+import os
 class Config:
     '''
     General configuration parent class
     '''
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
+
+
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
     pass
 
 
