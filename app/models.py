262c89269f31b17c4342d18aa922b30253c054f2
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-19 18:24:34 +0300

    Add class Comment

--- app/models.py
+++ app/models.py
@@ -44,3 +44,16 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name}' 
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(500))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch = db.Column(db.Integer,db.ForeignKey("pitches.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
