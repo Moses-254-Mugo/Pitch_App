6a8a09487a39a0250a83eadc0c3aeb496e00619f
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-19 10:34:02 +0300

    Add route that handles prifile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, redirect, url_for
+from flask import render_template,request, redirect, url_for,abort
 from . import main
 from .forms import ReviewForm
 from flask_login import login_required
+from ..models import Reviews, User
 
 # Views
 @main.route('/')
@@ -16,10 +17,11 @@ def index():
     return render_template('index.html', message = message, title = title)
 
 
-@main.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
 
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('movie.html')
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
