ab6fd52c7aac932745bd36958fe27d426c886115
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-19 13:09:17 +0300

    Modify file

--- app/main/views.py
+++ app/main/views.py
@@ -4,6 +4,7 @@ from .forms import ReviewForm,UpdateProfile
 from flask_login import login_required,current_user
 from ..models import User
 from .. import db,photos
+import markdown2
 
 # Views
 @main.route('/')
